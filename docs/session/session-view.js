0350c6aad579e4d35ad180cd4c17ccb4d5874b2e
Jorge <barrosogalindojorge@gmail.com>
2026-03-14 01:37:14 +0100

    Removing /docs from links

--- docs/session/session-view.js
+++ docs/session/session-view.js
@@ -2,10 +2,10 @@
 
 export function buildAuthorizedNav(){
     return `
-    <li><a class= "link active" href="/docs/index.html">Home </a></li>
+    <li><a class= "link active" href="index.html">Home </a></li>
     <li><a class= "link" href="#about">About Us</a></li>
-    <li><a class="link" href="/docs/listas_template.html">Lists</a></li>
-    <li><a class="link" href="/docs/creacionLista.html">New List</a></li>
+    <li><a class="link" href="listas_template.html">Lists</a></li>
+    <li><a class="link" href="creacionLista.html">New List</a></li>
     <li> <a class="link" id="logout-link">Log Out</a></li>
     <li><a class="link hidden-btn">Lang</a></li>
     `
@@ -13,10 +13,10 @@ export function buildAuthorizedNav(){
 
 export function buildUnauthorizedNav(){
         return `
-    <li><a class= "link active" href="/docs/index.html">Home </a></li>
+    <li><a class= "link active" href="index.html">Home </a></li>
     <li><a class= "link" href="#about">About Us</a></li>
-    <li><a class="link" href="/docs/listas_template.html">Lists</a></li>
-    <li><a class="link" href="/docs/login.html">Login</a></li>
+    <li><a class="link" href="listas_template.html">Lists</a></li>
+    <li><a class="link" href="login.html">Login</a></li>
     <li><a class="link hidden-btn">Lang</a></li>
 
     `
@@ -25,14 +25,14 @@ export function buildUnauthorizedNav(){
 
 export function buildAuthorizedButton(){
     return `
-        <a class="main-btn" href="/docs/creacionLista.html" class="btn">Make your Own Personal Lists</a>
+        <a class="main-btn" href="creacionLista.html" class="btn">Make your Own Personal Lists</a>
     `
 }
 
 export function buildUnauthorizedButton(){
     return `
       
-        <a class="main-btn" href="/docs/SignUp.html">Register to create your own List yourself!</a>
+        <a class="main-btn" href="SignUp.html">Register to create your own List yourself!</a>
        
     `
 }
