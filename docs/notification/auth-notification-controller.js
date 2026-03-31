316a6b6f199b9a1c766641ac4552ab2e752e5cec
Jorge <barrosogalindojorge@gmail.com>
2026-03-31 18:49:42 +0200

    its working

--- docs/notification/auth-notification-controller.js
+++ docs/notification/auth-notification-controller.js
@@ -3,6 +3,7 @@ import { buildRegisterNotification } from "./auth-view.js"
 export function registerNotification(signupForm,errors){
     for( const error of errors){
     const inputForm = signupForm.querySelector(`#${error.path}`)
+    if(!inputForm) continue
     const parentInput = inputForm.parentElement
     const notification = buildRegisterNotification(error)
     console.log(error.path)
